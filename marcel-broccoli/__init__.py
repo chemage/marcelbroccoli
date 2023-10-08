0393c69c14e8ef57d6a171c9618f5920a2ae8a42
Marcel Gerber <9885015+chemage@users.noreply.github.com>
2023-10-08 19:12:02 +0200

    rename folders

--- marcel-broccoli/__init__.py
+++
@@ -1,3 +0,0 @@
-from .errorcodes import ErrorCodes, errorcode
-from .functions import *
-from .logger import *
--- marcel-broccoli/version.py
+++
@@ -1,4 +0,0 @@
-# meta data
-__author__ = "Marcel Gerber"
-__date__ = "2023-10-08"
-__version__ = "1.0.0"
