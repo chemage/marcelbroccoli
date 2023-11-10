eaf1f82345ccfe786897349c53a644bfffd4b2fa
Marcel Gerber (BNC) <92036000+bncmge@users.noreply.github.com>
2023-11-10 15:27:14 +0100

    update version 1.5.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="marcelbroccoli",
-    version="1.4.2",
+    version="1.5.0",
     description="Marcel's custom functions",
     url="https://github.com/chemage/marcelbroccoli",
     author="Marcel Gerber",
