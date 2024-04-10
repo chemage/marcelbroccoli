f3edbbbc1d4bd4783754b47aedaaad7c4b799a9e
Marcel Gerber <marcel.gerber@bnc.ch>
2024-04-10 13:34:55 +0200

    Add function load_config_file

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="marcelbroccoli",
-    version="1.5.1",
+    version="1.6.0",
     description="Marcel's custom functions",
     url="https://github.com/chemage/marcelbroccoli",
     author="Marcel Gerber",
